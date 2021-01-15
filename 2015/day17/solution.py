b240e72dd7123d76c708b5b4d250a9fe22884db9
Ashyaa <heimdall409@gmail.com>
2021-01-15 19:23:09 +0100

    2015: day 17 optimizations

--- 2015/day17/solution.py
+++ 2015/day17/solution.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import contextlib
+from collections import defaultdict
 
 from pathlib import Path
+from numpy import vectorize
 from typing import List, Tuple
 from AoC.util import show
 
@@ -18,7 +20,7 @@ def read_input(filename: str="input.txt") -> List[Tuple[int, int]]:
         return [(c, i) for i, c in enumerate(r)]
 
 
-def candidates(cs: List[Tuple[int, int]], total: int=150, recursed=False):
+def candidates(cs: List[Tuple[int, int]], total: int=150, recursed: bool=False):
     for i, c in enumerate(cs):
         left = total - c[0]
         if left < 0:
@@ -34,7 +36,8 @@ def candidates(cs: List[Tuple[int, int]], total: int=150, recursed=False):
 
 
 @show
-def solve(containers: List[int], target: int=150) -> int:
+def solve(containers: List[Tuple[int, int]], target: int=150) -> Tuple[int, int]:
+    """Optimized solution based on candidate generator"""
     combinations = candidates(containers, target)
     res = set()
     for combi in combinations:
@@ -42,6 +45,17 @@ def solve(containers: List[int], target: int=150) -> int:
         res.add(tuple(combi))
     min_nb = min([len(s) for s in res])
     return len(res), len([s for s in res if len(s) == min_nb])
+
+
+@show
+def quick_solve() -> Tuple[int, int]:
+    """Optimized solution using bitmasks to represent the input"""
+    dimensions = vectorize(int)(list(open('input.txt')))
+    res = defaultdict(int)
+    for mask in range(1, 1<<len(dimensions)):
+        p = [d for i,d in enumerate(dimensions) if (mask & (1 << i)) > 0]
+        if sum(p) == 150: res[len(p)] += 1
+    return sum(res.values()), res[min(res.keys())]
 
 
 def test_example() -> None:
@@ -52,4 +66,5 @@ def test_example() -> None:
 
 test_example()
 containers = read_input()
+quick_solve() # 1638, 17
 solve(containers) # 1638, 17
