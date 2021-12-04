fa8d4324da6808c95303cf1c15b6a09a4107b294
Ashyaa <heimdall409@gmail.com>
2021-12-04 11:17:01 +0100

    2021: day 4 cleanup

--- 2021/day04/solution.py
+++ 2021/day04/solution.py
@@ -2,7 +2,7 @@
 
 import contextlib
 from pathlib import Path
-from typing import List, Tuple
+from typing import List
 
 import numpy as np
 from AoC.util import show
@@ -10,7 +10,27 @@ from AoC.util import show
 CWD = Path(__file__).parent
 
 
-def read_input(filename: str = "input.txt") -> Tuple[List[int], List[np.ndarray]]:
+def play(numbers: List[int], grids: List[np.ndarray]) -> List[int]:
+    res = []
+    for grid in grids:
+        won = False
+        for i in range(5, len(numbers)):
+            cur_numbers = set(numbers[:i])
+            for j in range(5):
+                if set(grid[:, j]).issubset(cur_numbers) or set(grid[j, :]).issubset(
+                    cur_numbers
+                ):
+                    res.append(
+                        (i, sum(set(grid.flatten()) - cur_numbers) * numbers[i - 1])
+                    )
+                    won = True
+                    break
+            if won:
+                break
+    return [score for _, score in sorted(res, key=lambda tup: tup[0])]
+
+
+def read_input(filename: str = "input.txt") -> List[int]:
     input_file = CWD.joinpath(filename)
     with open(input_file, "r") as reader:
         numbers = [int(w) for w in reader.readline().split(",")]
@@ -20,49 +40,28 @@ def read_input(filename: str = "input.txt") -> Tuple[List[int], List[np.ndarray]
             ).reshape(5, 5)
             for gr in "".join(reader.readlines()[1:]).split("\n\n")
         ]
-        return numbers, grids
+        return play(numbers, grids)
 
 
 @show
-def first(numbers: List[int], grids: List[np.ndarray]) -> int:
-    for i in range(5, len(numbers)):
-        cur_numbers = set(numbers[:i])
-        for grid in grids:
-            for j in range(5):
-                if set(grid[:, j]).issubset(cur_numbers) or set(grid[j, :]).issubset(
-                    cur_numbers
-                ):
-                    return sum(set(grid.flatten()) - cur_numbers) * numbers[i - 1]
-    return 0
+def first(scores: List[int]) -> int:
+    return scores[0]
 
 
 @show
-def second(numbers: List[int], grids: List[np.ndarray]) -> int:
-    def winning_when(numbers: List[int], grid: np.ndarray) -> int:
-        for i in range(5, len(numbers)):
-            cur_numbers = set(numbers[:i])
-            for j in range(5):
-                if set(grid[:, j]).issubset(cur_numbers) or set(grid[j, :]).issubset(
-                    cur_numbers
-                ):
-                    return i
-        return -1
-
-    order = [winning_when(numbers, grid) for grid in grids]
-    last_i = max(order)
-    last_grid = grids[order.index(last_i)]
-    return sum(set(last_grid.flatten()) - set(numbers[:last_i])) * numbers[last_i - 1]
+def second(scores: List[int]) -> int:
+    return scores[-1]
 
 
 def test_example() -> None:
     with contextlib.redirect_stdout(None):
-        i1, i2 = read_input("example.txt")
-        assert first(i1, i2) == 4512
-        assert second(i1, i2) == 1924
+        scores = read_input("example.txt")
+        assert first(scores) == 4512
+        assert second(scores) == 1924
 
 
 if __name__ == "__main__":
     test_example()
-    i1, i2 = read_input()
-    first(i1, i2)  # 33348
-    second(i1, i2)  # 8112
+    scores = read_input()
+    first(scores)  # 33348
+    second(scores)  # 8112
