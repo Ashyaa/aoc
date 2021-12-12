b489273d6093b1e9d58a08165286ee021ebd08f8
Ashyaa <heimdall409@gmail.com>
2021-12-12 11:19:54 +0100

    2021: day 12 cleanup

--- 2021/day12/solution.py
+++ 2021/day12/solution.py
@@ -16,67 +16,51 @@ def read_input(filename: str = "input.txt") -> Dict[str, List[str]]:
     input_file = CWD.joinpath(filename)
     with open(input_file, "r") as reader:
         res = {}
+        def add_edge(x,y):
+            if x in res:
+                res[x].append(y)
+            else:
+                res[x] = [y]
         for l in reader.readlines():
-            n1, n2 = l.split("-")[0], l.strip().split("-")[1]
-            if n1 in res:
-                res[n1].append(n2)
-            else:
-                res[n1] = [n2]
-            if n2 in res:
-                res[n2].append(n1)
-            else:
-                res[n2] = [n1]
+            n1, n2 = l.strip().split("-", maxsplit=1)
+            add_edge(n1, n2)
+            add_edge(n2, n1)
         return res
 
 
-def path(graph: Dict[str, List[str]], cur_node: str, visited: Dict[str, int]) -> List[List[str]]:
-    res = []
+def path(graph: Dict[str, List[str]], cur_node: str, visited: Set[str], part2: bool = False) -> int:
     if cur_node == "end":
-        return [[cur_node]]
-    if cur_node.islower():
-        visited[cur_node] += 1
+        return 1
+    res = 0
     for node in graph[cur_node]:
-        if visited[node] < 2:
-            pths =  path(graph, node, visited.copy())
-            for p in pths:
-                res.append([cur_node] + p)
-    if not res:
-        res = [[cur_node]]
+        if node.islower():
+            if node not in visited:
+                res += path(graph, node, visited | {node}, part2)
+            elif part2 and node not in {"start", "end"}:
+                res += path(graph, node, visited | {node}, False)
+        else:
+            res += path(graph, node, visited, part2)
     return res
 
 
 @show
 def first(inp: Dict[str, List[str]]) -> int:
-    visited = {node: int(node.islower()) for node in inp}
-    return len([p for p in path(inp, "start", visited) if p[-1] == "end"])
+    return path(inp, "start", {"start"})
 
 
 @show
 def second(inp: Dict[str, List[str]]) -> int:
-    visited = {node: int(node.islower()) for node in inp}
-    res = []
-    for k in [k for k in visited if k.islower()]:
-        if k == "start" or k == "end": continue
-        vis2 = visited.copy()
-        vis2[k] = 0
-        tmp = [p for p in path(inp, "start", vis2) if p[-1] == "end"]
-        for p in tmp:
-            pth = "".join(p)
-            if pth not in res:
-                res.append(pth)
-    return len(res)
+    return path(inp, "start", {"start"}, True)
 
 
 def test_example() -> None:
+    inp = read_input("example.txt")
     with contextlib.redirect_stdout(None):
-        inp = read_input("example.txt")
         assert first(inp) == 10
         assert second(inp) == 36
-    with contextlib.redirect_stdout(None):
         inp = read_input("example_2.txt")
         assert first(inp) == 19
         assert second(inp) == 103
-    with contextlib.redirect_stdout(None):
         inp = read_input("example_3.txt")
         assert first(inp) == 226
         assert second(inp) == 3509
@@ -86,4 +70,4 @@ if __name__ == "__main__":
     test_example()
     inp = read_input()
     first(inp)  # 4720
-    second(inp)  # p2
+    second(inp)  # 147848
