75f41cf5b2e5b240fe23cdbe777914ff062125c9
Ashyaa <heimdall409@gmail.com>
2021-12-15 23:13:08 +0100

    2021: day 15 minor cleanup

--- 2021/day15/solution.py
+++ 2021/day15/solution.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 import contextlib
-
 from pathlib import Path
-import numpy as np
-
 from typing import *
-from AoC.util import show
 
+import numpy as np
+from AoC.util import show
 
 CWD = Path(__file__).parent
 
@@ -17,7 +15,7 @@ def read_input(filename: str = "input.txt") -> np.ndarray:
     return np.genfromtxt(input_file, delimiter=1, dtype=int)
 
 
-def neighbours(arr: np.ndarray, coord: Tuple[int, int]) -> List[Tuple[int, int]]:
+def neighbours(arr: np.ndarray, coord: Tuple[int, int]) -> Generator[Tuple[int, int], None, None]:
     x, y = coord
     for delta_x, delta_y in [(-1,0), (1,0), (0,-1), (0,1)]:
         new_x, new_y = x + delta_x, y + delta_y
@@ -26,53 +24,49 @@ def neighbours(arr: np.ndarray, coord: Tuple[int, int]) -> List[Tuple[int, int]]
 
 
 def a_star(arr: np.ndarray, start: Tuple[int, int], stop: Tuple[int, int]) -> List[Tuple[int, int]]:
-    open_lst = set([start])
-    closed_lst = set([])
+    open_lst, closed_lst = set([start]), set([])
     risk_from_start = {start: 0}
-
-    # next_node contains an adjac mapping of all nodes
     next_node = {start: start}
 
     while len(open_lst) > 0:
-        n = None
+        node = None
         for coord in open_lst:
-            if n is None or risk_from_start[coord] + arr[coord] <= risk_from_start[n] + arr[n]:
-                n = coord
+            if node is None or risk_from_start[coord] + arr[coord] <= risk_from_start[node] + arr[node]:
+                node = coord
 
-        if n == stop:
+        if node == stop:
             reconst_path = []
-
-            while next_node[n] != n: # start condition
-                reconst_path.append(n)
-                n = next_node[n]
+            while next_node[node] != node: # start condition
+                reconst_path.append(node)
+                node = next_node[node]
             reconst_path.reverse()
             return reconst_path
 
-        for m in neighbours(arr, n):
-            risk = arr[m]
-            if m not in open_lst and m not in closed_lst:
-                open_lst.add(m)
-                next_node[m] = n
-                risk_from_start[m] = risk_from_start[n] + risk
+        for nbr in neighbours(arr, node):
+            risk = arr[nbr]
+            if nbr not in open_lst and nbr not in closed_lst:
+                open_lst.add(nbr)
+                next_node[nbr] = node
+                risk_from_start[nbr] = risk_from_start[node] + risk
             else:
-                if risk_from_start[m] > risk_from_start[n] + risk:
-                    risk_from_start[m] = risk_from_start[n] + risk
-                    next_node[m] = n
-                    if m in closed_lst:
-                        closed_lst.remove(m)
-                        open_lst.add(m)
+                if risk_from_start[nbr] > risk_from_start[node] + risk:
+                    risk_from_start[nbr] = risk_from_start[node] + risk
+                    next_node[nbr] = node
+                    if nbr in closed_lst:
+                        closed_lst.remove(nbr)
+                        open_lst.add(nbr)
 
-        open_lst.remove(n)
-        closed_lst.add(n)
+        open_lst.remove(node)
+        closed_lst.add(node)
 
 
 @show
 def first(inp: np.ndarray) -> int:
     pth = a_star(inp, (0,0), (inp.shape[0]-1, inp.shape[1]-1))
-    return sum(inp[n] for n in pth)
+    return sum(inp[node] for node in pth)
 
 
-def tile_value(value: int, shift: int) -> np.ndarray:
+def tile_value(value: int, shift: int) -> int:
     res = value
     for _ in range(shift):
         res = res + 1 if res < 9 else 1
@@ -93,7 +87,7 @@ def build_map(inp: np.ndarray) -> np.ndarray:
 def second(inp: np.ndarray) -> int:
     arr = build_map(inp)
     pth = a_star(arr, (0,0), (arr.shape[0]-1, arr.shape[1]-1))
-    return sum(arr[n] for n in pth)
+    return sum(arr[node] for node in pth)
 
 
 def test_example() -> None:
