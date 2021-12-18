8a8537f6b468f4a04af79df4258c93b9d35d294f
Ashyaa <heimdall409@gmail.com>
2021-12-18 23:28:43 +0100

    2021: day 18 improved

--- 2021/day18/solution.py
+++ 2021/day18/solution.py
@@ -26,83 +26,81 @@ class Snailfish:
         return res
 
 
-    def will_explode(self) -> bool:
-        lvl = 0
-        for c in str(self):
-            if c == '[':
-                lvl +=1
-            elif c == ']':
-                lvl -=1
-            if lvl > 4:
-                return True
-        return False
-
-
-    def explode(self):
-        lvl = 0
-        s = str(self)
+    def explode(self) -> bool:
+        lvl,s = 0, str(self)
+        stack, st_l, st_exp = [], [], []
         begin, end = -1, -1
         for i, c in enumerate(s):
             if c == '[':
-                lvl +=1
-                if lvl == 5:
+                lvl += 1
+                stack.append(False)
+                if lvl == 5 and begin < 0:
+                    st_exp = stack[:-1]
                     begin = i
             elif c == ']':
-                if lvl == 5:
+                stack = stack[:-1]
+                if lvl == 5 and end < 0:
                     end = i
+                lvl -= 1
+            elif c == ',':
+                stack[-1] = True
+            else:
+                if end > 0:
                     break
-                lvl -=1
-        left_str, right_str = s[:begin], s[end+1:]
+                if begin < 0:
+                    st_l = stack.copy()
+        if begin < 0:
+            return False
         l_val, r_val = [int(n) for n in s[begin+1:end].split(',')]
-        new_str = add_to_str(left_str, l_val, True) + "0" + add_to_str(right_str, r_val, False)
-        new_self = from_str(new_str)
-        self.l = new_self.l
-        self.r = new_self.r
+        self.set_val(st_l, l_val, True)
+        self.set_val(st_exp, 0, False)
+        self.set_val(stack, r_val, True)
+        return True
 
 
-    def will_split(self) -> bool:
-        return any(int(n) >= 10 for n in str(self).replace("[", ",").replace("]", ",").split(",") if n != '')
-
-        lsplit, rsplit = False, False
-        if isinstance(self.l, int):
-            if self.l >= 10:
-                lsplit = True
+    def set_val(self, stack: List[bool], val, add: bool):
+        if not stack:
+            return
+        sn = self
+        for s in stack[:-1]:
+            sn = sn.r if s else sn.l
+        if stack[-1]:
+            if add:
+                sn.r += val
+            else:
+                sn.r = val
         else:
-            lsplit = self.l.will_split()
-        if isinstance(self.r, int):
-            if self.r >= 10:
-                rsplit = True
-        else:
-            rsplit = self.r.will_split()
-        return lsplit or rsplit
+            if add:
+                sn.l += val
+            else:
+                sn.l = val
 
 
-    def split(self):
+    def split(self) -> bool:
+        res = False
         if isinstance(self.l, int):
             if self.l >= 10:
                 self.l = Snailfish(self.l // 2, self.l - (self.l // 2))
-                return
-        elif self.l.will_split():
-            self.l.split()
-            return
-        if isinstance(self.r, int):
-            if self.r >= 10:
-                self.r = Snailfish(self.r // 2, self.r - (self.r // 2))
-                return
-        elif self.r.will_split():
-            self.r.split()
+                return True
+        else:
+            res = self.l.split()
+        if not res:
+            if isinstance(self.r, int):
+                if self.r >= 10:
+                    self.r = Snailfish(self.r // 2, self.r - (self.r // 2))
+                    return True
+            else:
+                return self.r.split()
+        return res
 
 
     def reduce(self):
-        reduced = False
-        while not reduced:
-            if self.will_explode():
-                self.explode()
+        while True:
+            if self.explode():
                 continue
-            if self.will_split():
-                self.split()
+            if self.split():
                 continue
-            reduced = True
+            break
 
 
     def magnitude(self) -> int:
@@ -115,71 +113,53 @@ class Snailfish:
         return f"[{str(self.l)},{str(self.r)}]"
 
 
-def add_to_str(s: str, v: int, left: bool) -> str:
-    values = [int(n) for n in s.replace("[", ",").replace("]", ",").split(",") if n != '']
-    if not values:
-        return s
-    wanted = values[-1] if left else values[0]
-    new_value = wanted + v
-    if left:
-        return (s[::-1].replace(str(wanted)[::-1], str(new_value)[::-1], 1))[::-1]
-    return s.replace(str(wanted), str(new_value), 1)
-
-
-def is_valid(s: str) -> bool:
-    return s.count('[') == s.count(']')
-
-
 def cast(s: str) -> Union[int, Snailfish]:
-    try:
-        return int(s)
-    except ValueError:
+    if ',' in s:
         return from_str(s)
+    return int(s)
 
 
 def from_str(s: str) -> Snailfish:
-    l, r = "", ""
     s = s[1:-1]
+    i, lvl = 0, 0
     for i, c in enumerate(s):
-        if c != ",":
-            continue
-        l, r = s[:i], s[i+1:]
-        if is_valid(l) and is_valid(r):
+        if c == '[':
+            lvl += 1
+        elif c == ']':
+            lvl -= 1
+        if c == "," and lvl == 0:
             break
-    return Snailfish(cast(l), cast(r))
+    return Snailfish(cast(s[:i]), cast(s[i+1:]))
 
 
-def read_input(filename: str = "input.txt") -> List[Snailfish]:
+def read_input(filename: str = "input.txt") -> List[str]:
     input_file = CWD.joinpath(filename)
     with open(input_file, "r") as reader:
-        return [from_str(l.strip()) for l in reader.readlines()]
+        return [l.strip() for l in reader.readlines()]
 
 
 @show
-def first(inp: List[Snailfish]) -> int:
-    return reduce(lambda x,y: x+y, inp).magnitude()
+def first(inp: List[str]) -> int:
+    return reduce(lambda x,y: x+y, [from_str(l) for l in inp]).magnitude()
 
 
 @show
-def second(inp) -> int:
+def second(inp: List[str]) -> int:
     res = []
     for i in range(len(inp)-1):
-        for j in range(i, len(inp)):
-            res.append((inp[i]+inp[j]).magnitude())
-            res.append((inp[j]+inp[i]).magnitude())
+        for j in range(i+1, len(inp)):
+            res.append((from_str(inp[i])+from_str(inp[j])).magnitude())
+            res.append((from_str(inp[j])+from_str(inp[i])).magnitude())
     return max(res)
 
 
 def test_example() -> None:
-    assert add_to_str(",1],2],3],4]", 8, False) == ",9],2],3],4]"
-    assert add_to_str("[7,[6,[5,[14,", 3, True) == "[7,[6,[5,[17,"
     n = from_str("[[[[[9,8],1],2],3],4]")
     n.explode()
     assert str(n) == "[[[[0,9],2],3],4]"
     n = from_str("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
     n.explode()
     assert str(n) == "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
-    assert str(from_str("[[[[4,3],4],4],[7,[[8,4],9]]]") + from_str("[1,1]")) == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
     assert str(from_str("[[[[4,3],4],4],[7,[[8,4],9]]]") + from_str("[1,1]")) == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
     assert from_str("[9,1]").magnitude() == 29
     assert from_str("[1,9]").magnitude() == 21
