18c92f10d708792cab63c5794aacfbb8273de757
Ashyaa <heimdall409@gmail.com>
2022-12-05 09:38:20 +0100

    2022: day 02 minor improvements

--- 2022/day02/solution.py
+++ 2022/day02/solution.py
@@ -10,11 +10,7 @@ from AoC.util import show
 CWD = Path(__file__).parent
 
 ITEMS = ["A","B","C"]
-
 PLAYS = ["X","Y","Z"]
-
-
-get_index = lambda l, item: next(i for i, it in enumerate(l) if it == item)
 
 
 def read_input(filename: str = "input.txt") -> List[List[str]]:
@@ -23,8 +19,8 @@ def read_input(filename: str = "input.txt") -> List[List[str]]:
 
 
 def score_1(p1: str, p2: str) -> int:
-    index_p1 = get_index(ITEMS, p1)
-    index_p2 = get_index(PLAYS, p2)
+    index_p1 = ITEMS.index(p1)
+    index_p2 = PLAYS.index(p2)
     if index_p2 == index_p1:
         return 3 + index_p2+1
     if (index_p2-1) % 3 == index_p1:
@@ -33,10 +29,10 @@ def score_1(p1: str, p2: str) -> int:
 
 
 def score_2(p1: str, p2: str) -> int:
-    index_p1 = get_index(ITEMS, p1)
-    index_p2 = get_index(PLAYS, p2)
+    index_p1 = ITEMS.index(p1)
+    index_p2 = PLAYS.index(p2)
     to_play = (index_p1 + index_p2 - 1) % 3
-    return 1 + get_index(ITEMS, ITEMS[to_play]) + index_p2 * 3
+    return 1 + ITEMS.index(ITEMS[to_play]) + index_p2 * 3
 
 
 @show
