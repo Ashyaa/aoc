df8d4c2f5d40c4e5d0e1f747f7f5316e692d9542
Ashyaa <heimdall409@gmail.com>
2022-12-23 15:55:30 +0100

    2022: day 22 fully automated

--- 2022/day22/solution.py
+++ 2022/day22/solution.py
@@ -9,29 +9,9 @@ from AoC.util import show
 
 CWD = Path(__file__).parent
 ROTATE = lambda dir, right: (dir + 1) % 4 if right else (dir - 1) % 4
-MOVES: List[Tuple[int,int]] = [
-    (0, 1), # RIGHT
-    (1, 0), # DOWN
-    (0, -1), # LEFT
-    (-1, 0), # UP
-]
 IN_BOUNDS = lambda x,y,map: 0 <= x < len(map) and 0 <= y < len(map[0])
-WARP = [ #TODO: generate structure with mapping function
-    (9, 1),
-    (8, 0),
-    (5, 2),
-    (4, 1),
-    (3, -1),
-    (2, 2),
-    (7, 1),
-    (6, -1),
-    (1, 0),
-    (0, -1),
-    (13, 2),
-    (12, 1),
-    (11, -1),
-    (10, 2),
-]
+MOVES: List[Tuple[int,int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+UNMATCHED = (-1,-1)
 
 
 def read_input(filename: str = "input.txt") -> Tuple[List[str], str]:
@@ -74,38 +54,42 @@ def layout(map: List[str]) -> Tuple[List[Tuple[int,int]],List[Tuple[int,int]], L
     return row, col, sides(map, row)
 
 
-def mapping(sides: List) -> List:
-    res = [(-1,-1,-1) for _ in sides] # matching side, reversed, rotation
+def new_dir(side: List[Tuple[int,int]]) -> int:
+    x1, y1 = side[0]
+    x2, y2 = side[-1]
+    if x1 == x2:
+        return 1 if y1 < y2 else 3
+    return 2 if x1 < x2 else 0
+
+
+def mapping(sides: List[List[Tuple[int,int]]]) -> List[Tuple[int,int]]:
+    res = [UNMATCHED for _ in sides]
     for i, side in enumerate(sides):
-        if res[i] != (-1,-1,-1):
+        j = (i + 1) % 14
+        if res[i] != UNMATCHED or res[j] != UNMATCHED:
             continue
-        p2 = side[-1]
-        j = (i+1) % 14
-        c1, c2 = (0,0,0), (1,0,0)
-        flip = False
-        while j != i:
-            other_side = sides[j]
-            nc1 = c2
-            # FIX: handle cube flip properly to match sides
-            if dist(*other_side[0], *p2) == 2:
-                nc2 = c1
-                flip = not flip
-            elif other_side[0] == p2: # same face
-                nc2 = (c2[1], int(not c2[0]), c2[2]) if flip else (int(not c2[1]), c2[0], c2[2])
-            else:
-                nc2 = int(not c2[2]), c2[1], c2[0]
-                nc2 = (c2[2], c2[1], int(not c2[0])) if flip else (int(not c2[2]), c2[1], c2[0])
-            if nc1 == (0,0,0) and nc2 == (1,0,0):
-                res[i] = (j, 0, 0)
-                res[j] = (i, 0, 0)
-                break
-            elif nc1 == (1,0,0) and nc2 == (0,0,0):
-                res[i] = (j, 1, 0)
-                res[j] = (i, 1, 0)
-                break
-            p2 = other_side[-1]
-            c1, c2 = nc1, nc2
-            j = (j + 1) % 14
+        next_side = sides[j]
+        if dist(*side[-1], *next_side[0]) == 2:
+            res[i] = (j, new_dir(next_side))
+            res[j] = (i, new_dir(side))
+            h, k = (i-1) % 14, (j+1) % 14
+            prev_side, next_next_side = sides[h], sides[k]
+            d1, d2 = dist(*prev_side[-1], *side[0]), dist(*next_side[-1], *next_next_side[0])
+            if (d1, d2) == (0,1) or (d2, d1) == (0,1):
+                res[h] = (k, new_dir(next_next_side))
+                res[k] = (h, new_dir(prev_side))
+    for i in range(14):
+        if res[i] != UNMATCHED:
+                continue
+        j = (i + 1) % 14
+        op = lambda n: (n+1) % 14
+        if res[j] == UNMATCHED:
+            j = (i - 1) % 14
+            op = lambda n: (n-1) % 14
+        while j != i and res[j] != UNMATCHED:
+            j = op(j)
+        res[i] = (j, new_dir(sides[j]))
+        res[j] = (i, new_dir(sides[i]))
     return res
 
 
@@ -141,11 +125,10 @@ def sides(map: List[str], row: List[Tuple[int,int]]) -> List[List[Tuple[int,int]
         else:
             begin, end, step = (p1_x, p2_x+1, 1) if p1_x < p2_x else (p1_x, p2_x-1, -1)
             res.append([(x, p1_y) for x in range(begin, end, step)])
-    # mapping(res)
     return res
 
 
-def move(x: int, y: int, dx: int, dy: int, mi: int, ma: int, map: List[str]) -> Tuple[int,int]:
+def move_p1(x: int, y: int, dx: int, dy: int, mi: int, ma: int, map: List[str]) -> Tuple[int,int]:
     nx, ny = x+dx, y+dy
     if dx == 0:
         ny = mi if ny > ma else ny
@@ -158,36 +141,19 @@ def move(x: int, y: int, dx: int, dy: int, mi: int, ma: int, map: List[str]) -> 
     return nx, ny
 
 
-def dist(a,b, c, d) -> int:
+def dist(a: int, b: int, c: int, d: int) -> int:
     return abs(a-c) + abs(b-d)
 
 
-def warp(x: int, y: int, dir: int, sides: List[List[Tuple[int,int]]], cheat: bool):
-    cur_side_idx = next(i for i, side in enumerate(sides) if (x,y) in side and (dir % 2) == (side[0][0] == side[-1][0])) 
+def warp(x: int, y: int, dir: int, sides: List[List[Tuple[int,int]]], warps: List[Tuple[int,int]]):
+    cur_side_idx = next(i for i, side in enumerate(sides) if (x,y) in side and (dir % 2) == (side[0][0] == side[-1][0]))
     pos_idx = sides[cur_side_idx].index((x,y))
-    if cheat:
-        new_side, rot = WARP[cur_side_idx]
-        new_dir = (dir + rot) % 4
-        nx, ny =  sides[new_side][-pos_idx-1]
-        return nx, ny, new_dir
-    step, i = 0, 1
-    new_dir = dir 
-    while i != 0:
-        next_side_idx = (cur_side_idx + 1) % 14
-        p1 = sides[cur_side_idx][-1]
-        p2 = sides[next_side_idx][0]
-        if p1 == p2:
-            i += 1
-        else:
-            i -= 1
-            new_dir = (new_dir + 1) % 4
-        cur_side_idx = next_side_idx
-        step += 1
-    nx, ny = sides[cur_side_idx][-pos_idx-1]
+    new_side, new_dir = warps[cur_side_idx]
+    nx, ny =  sides[new_side][-pos_idx-1]
     return nx, ny, new_dir
 
 
-def move_2(x: int, y: int, dir: int, map: List[str], sides: List[List[Tuple[int,int]]], cheat: bool) -> Tuple[int,int,int]:
+def move_p2(x: int, y: int, dir: int, map: List[str], sides: List[List[Tuple[int,int]]], warps: List[Tuple[int,int]]) -> Tuple[int,int,int]:
     dx, dy = MOVES[dir]
     nx, ny = x+dx, y+dy
     if IN_BOUNDS(nx, ny, map):
@@ -195,15 +161,16 @@ def move_2(x: int, y: int, dir: int, map: List[str], sides: List[List[Tuple[int,
             return nx, ny, dir
         elif map[nx][ny] == "#":
             return x, y, dir
-    nx, ny, ndir = warp(x, y, dir, sides, cheat)
+    nx, ny, ndir = warp(x, y, dir, sides, warps)
     if map[nx][ny] == "#":
         return x, y, dir
     return nx, ny, ndir
 
 
 @show
-def first(map: List[str], path: str) -> int:
-    rows, cols, _ = layout(map)
+def run(map: List[str], path: str, p2: bool = False) -> int:
+    rows, cols, sides = layout(map)
+    warps = mapping(sides)
     dir, i = 0, 0
     x, y = 0, rows[0][0]
     while map[x][y] != ".":
@@ -218,36 +185,14 @@ def first(map: List[str], path: str) -> int:
         while i < len(path) and path[i].isdigit():
             raw += path[i]
             i += 1
-        n = int(raw)
         mv = MOVES[dir]
         mi, ma = rows[x] if mv[0] == 0 else cols[y]
-        for _ in range(n):
-            nx, ny = move(x, y, *mv, mi, ma, map)
-            if (x,y) == (nx,ny):
-                break
-            x, y = nx,ny
-    return 1000 * (x+1) + 4 * (y+1) + dir
-
-
-@show
-def second(map: List[str], path: str, cheat: bool = False) -> int:
-    rows, _, sides = layout(map)
-    dir, i = 0, 0
-    x, y = 0, rows[0][0]
-    while map[x][y] != ".":
-        y = y + 1
-    while i < len(path):
-        if path[i].isalpha():
-            c = path[i]
-            i += 1
-            dir = ROTATE(dir, c == "R")
-            continue
-        raw = ""
-        while i < len(path) and path[i].isdigit():
-            raw += path[i]
-            i += 1
         for _ in range(int(raw)):
-            nx, ny, ndir = move_2(x, y, dir, map, sides, cheat)
+            if p2:
+                nx, ny, ndir = move_p2(x, y, dir, map, sides, warps)
+            else:
+                nx, ny = move_p1(x, y, *mv, mi, ma, map)
+                ndir = dir
             if (x,y,dir) == (nx,ny,ndir):
                 break
             x, y, dir = nx, ny, ndir
@@ -257,13 +202,13 @@ def second(map: List[str], path: str, cheat: bool = False) -> int:
 def test_example() -> None:
     with contextlib.redirect_stdout(None):
         inp = read_input("example.txt")
-        r1  = first(*inp)
+        r1  = run(*inp)
         assert r1 == 6032, r1
-        r2  = second(*inp)
+        r2  = run(*inp, True)
         assert r2 == 5031, r2
 
 
 test_example()
 s = read_input()
-first(*s)  # 106094
-second(*s, cheat=True)  # 162038
+run(*s)  # 106094
+run(*s, True)  # 162038
