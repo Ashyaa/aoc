0d07d86d4803afad0c8a62e43563ea15efdc3d7d
Ashyaa <heimdall409@gmail.com>
2023-11-13 22:15:36 +0100

    2023: prepare Go code

---
+++ 2023/day01/solution_test.go
@@ -0,0 +1,38 @@
+package day01
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	R "github.com/stretchr/testify/require"
+)
+
+const (
+	inputFile   = "./input.txt"
+	exampleFile = "./example.txt"
+)
+
+// ReadInput retrieves the content of the input file
+func ReadInput(filepath string) []string {
+	data, _ := os.ReadFile(filepath)
+	return strings.Split(string(data), "\n")
+}
+
+func First(input []string) interface{} {
+	return nil
+}
+
+func Second(input []string) interface{} {
+	return nil
+}
+
+func TestDay(t *testing.T) {
+	r := R.New(t)
+	example := ReadInput(exampleFile)
+	input := ReadInput(inputFile)
+	r.Equal(nil, First(example))
+	r.Equal(nil, First(input))
+	r.Equal(nil, Second(example))
+	r.Equal(nil, Second(input))
+}
