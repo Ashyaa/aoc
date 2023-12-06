eb7a4e1df1c417e792e016e10898f5ec9e2230d1
Ashyaa <heimdall409@gmail.com>
2023-12-06 21:52:17 +0100

    2023: day 06 with the power of maths

--- 2023/day06/solution_test.go
+++ 2023/day06/solution_test.go
@@ -3,6 +3,7 @@ package day06
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -33,23 +34,22 @@ func ReadInput(filepath string) (res []int) {
 	return
 }
 
+// x (time - x) = distance
+// x*time - x² = distance
+// x² - x * time + distance = 0
+// by construction of the exercise, we know that D = time² - 4*distance > 0
+// so first root is (time - sqrt(D)) / 2
+func NbWins(time, distance int) int {
+	discriminant := float64(time*time - 4*distance)
+	root := math.Floor((float64(time) - math.Sqrt(discriminant)) / 2)
+	return 1 + time - 2*int(root+1)
+}
+
 func First(input []int) int {
 	res := 1
 	for i := 0; i < len(input); i += 2 {
 		time, distance := input[i], input[i+1]
-		nbWins := 0
-		for t := 1; t < (time+1)/2; t++ {
-			d := t * (time - t)
-			if d > distance {
-				nbWins += 1
-			}
-		}
-		nbWins *= 2
-		if time%2 == 0 {
-			nbWins += 1
-		}
-		res *= nbWins
-
+		res *= NbWins(time, distance)
 	}
 	return res
 }
@@ -62,17 +62,7 @@ func Second(input []int) (res int) {
 	}
 	time, _ := strconv.Atoi(timeStr)
 	distance, _ := strconv.Atoi(distanceStr)
-	for t := 1; t < (time+1)/2; t++ {
-		d := t * (time - t)
-		if d > distance {
-			res += 1
-		}
-	}
-	res *= 2
-	if time%2 == 0 {
-		res += 1
-	}
-	return res
+	return NbWins(time, distance)
 }
 
 func TestDay06(t *testing.T) {
