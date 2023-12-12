d239e893f0de34eb9101e1289c7f6c737d282677
Ashyaa <heimdall409@gmail.com>
2023-12-12 18:07:30 +0100

    2023: day 12 WIP

--- 2023/day12/solution_test.go
+++ 2023/day12/solution_test.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -26,17 +28,96 @@ func ReadInput(filepath string) (res []string) {
 	return
 }
 
+func isValid(buffer []rune, counts []int) bool {
+	fields := strings.Fields(string(buffer))
+	if len(fields) != len(counts) {
+		return false
+	}
+	for idx, f := range fields {
+		c := counts[idx]
+		if len(f) != c {
+			return false
+		}
+	}
+	return true
+}
+
+func missingIndexes(s string) (res []int) {
+	for idx, c := range s {
+		if c == '?' {
+			res = append(res, idx)
+		}
+	}
+	return
+}
+
+func newSprings(combination, nbBits int) (res []int, sum int) {
+	for shift := 0; shift < nbBits; shift++ {
+		mask := 1 << shift
+		bit := (combination & mask) >> shift
+		res = append(res, bit)
+		sum += bit
+	}
+	return
+}
+
+func compute(s, counts string) (res int) {
+	springs := []int{}
+	totalSprings := 0
+	for _, rawCount := range strings.Split(counts, ",") {
+		count, _ := strconv.Atoi(rawCount)
+		totalSprings += count
+		springs = append(springs, count)
+	}
+	curNbSprings := strings.Count(s, "#")
+	toFind := totalSprings - curNbSprings
+	missingIdxs := missingIndexes(s)
+	nbMissing := len(missingIdxs)
+	maxCombination := 1
+	for i := 1; i <= toFind; i++ {
+		maxCombination += 1 << (nbMissing - i)
+	}
+	for combination := (1 << toFind) - 1; combination < maxCombination; combination++ {
+		toAdd, sum := newSprings(combination, nbMissing)
+		if sum != toFind {
+			continue
+		}
+		buffer := []rune(strings.ReplaceAll(s, ".", " "))
+		for idx, newSpring := range toAdd {
+			newChar := ' '
+			if newSpring == 1 {
+				newChar = '#'
+			}
+			buffer[missingIdxs[idx]] = newChar
+		}
+		if isValid(buffer, springs) {
+			res += 1
+		}
+	}
+	return
+}
+
 func Solve(input []string) (p1 int, p2 int) {
+	for _, s := range input {
+		tmp := strings.Split(s, " ")
+		p1 += compute(tmp[0], tmp[1])
+		// s1, s2 := tmp[0], tmp[1]
+		// for i := 0; i < 4; i++ {
+		// 	s1 += "?" + tmp[0]
+		// 	s2 += "?" + tmp[1]
+		// }
+	}
 	return
 }
 
 func TestDay12(t *testing.T) {
 	r := R.New(t)
 	p1Ex, p2Ex := Solve(ReadInput(example))
-	r.Equal(0, p1Ex, "example p1")
+	r.Equal(21, p1Ex, "example p1")
+	// r.Equal(525152, p2Ex, "example p2")
 	r.Equal(0, p2Ex, "example p2")
 	p1, p2 := Solve(ReadInput(input))
-	r.Equal(0, p1, "input p1")
+	r.Equal(7236, p1, "input p1")
 	r.Equal(0, p2, "input p2")
 }
 
