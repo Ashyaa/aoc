5f43b5cc5cb073c16d5f6333dfb2e88c7b519388
Ashyaa <heimdall409@gmail.com>
2023-12-15 18:08:15 +0100

    2023: day 15 WIP

--- 2023/day15/solution_test.go
+++ 2023/day15/solution_test.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -21,23 +23,100 @@ func ReadInput(filepath string) (res []string) {
 	sc := bufio.NewScanner(s)
 	for sc.Scan() {
 		// parsing here...
-		res = append(res, sc.Text())
+		res = strings.Split(sc.Text(), ",")
+	}
+	return
+}
+
+func hash(s string) (res int) {
+	for _, c := range s {
+		res += int(c)
+		res *= 17
+		res %= 256
+	}
+	return
+}
+
+func step(boxes [][]string, step string, dict map[string]int) {
+	if strings.Contains(step, "-") {
+		s := strings.Replace(step, "-", "", 1)
+		if _, ok := dict[s]; !ok {
+			return
+		}
+		h := hash(s)
+		for idx, lens := range boxes[h] {
+			if strings.HasPrefix(lens, s) {
+				boxes[h][idx] = ""
+				break
+			}
+		}
+		delete(dict, s)
+		return
+	}
+	tmp := strings.Split(step, "=")
+	if h, ok := dict[tmp[0]]; ok {
+		for idx, lens := range boxes[h] {
+			if strings.HasPrefix(lens, tmp[0]) {
+				boxes[h][idx] = tmp[0] + " " + tmp[1]
+				break
+			}
+		}
+	} else {
+		h = hash(tmp[0])
+		boxes[h] = append(boxes[h], tmp[0]+" "+tmp[1])
+		dict[tmp[0]] = h
+	}
+}
+
+func score(boxes [][]string) (res int) {
+	for idx, b := range boxes {
+		slot := 0
+		for _, lens := range b {
+			if lens == "" {
+				continue
+			}
+			slot++
+			tmp := strings.Split(lens, " ")
+			focalLength, _ := strconv.Atoi(tmp[1])
+			res += (idx + 1) * slot * focalLength
+		}
 	}
 	return
 }
 
 func Solve(input []string) (p1 int, p2 int) {
+	boxes := make([][]string, 256)
+	dict := make(map[string]int)
+	for i := 0; i < 256; i++ {
+		boxes[i] = []string{}
+	}
+	for _, l := range input {
+		p1 += hash(l)
+		step(boxes, l, dict)
+	}
+	p2 = score(boxes)
 	return
 }
 
 func TestDay15(t *testing.T) {
 	r := R.New(t)
+	r.Equal(30, hash("rn=1"))
+	r.Equal(253, hash("cm-"))
+	r.Equal(97, hash("qp=3"))
+	r.Equal(47, hash("cm=2"))
+	r.Equal(14, hash("qp-"))
+	r.Equal(180, hash("pc=4"))
+	r.Equal(9, hash("ot=9"))
+	r.Equal(197, hash("ab=5"))
+	r.Equal(48, hash("pc-"))
+	r.Equal(214, hash("pc=6"))
+	r.Equal(231, hash("ot=7"))
 	p1Ex, p2Ex := Solve(ReadInput(example))
-	r.Equal(0, p1Ex, "example p1")
-	r.Equal(0, p2Ex, "example p2")
+	r.Equal(1320, p1Ex, "example p1")
+	r.Equal(145, p2Ex, "example p2")
 	p1, p2 := Solve(ReadInput(input))
-	r.Equal(0, p1, "input p1")
-	r.Equal(0, p2, "input p2")
+	r.Equal(517965, p1, "input p1")
+	r.Equal(0, p2, "input p2") // x < 270364
 }
 
 func BenchmarkDay15(b *testing.B) {
