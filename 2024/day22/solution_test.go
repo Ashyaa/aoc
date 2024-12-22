46270d6766fa1d816ba77faa98dc38f694f41369
Ashyaa <heimdall409@gmail.com>
2024-12-22 15:13:35 +0100

    2024: day 22

--- 2024/day22/solution_test.go
+++ 2024/day22/solution_test.go
@@ -1,9 +1,10 @@
 package day22
 
 import (
-	"bufio"
+	. "aoc/utils"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -13,31 +14,67 @@ import (
 const (
 	input   = "./input.txt"
 	example = "./example.txt"
+	p       = (1 << 24) - 1 // bitmask for 24 bits integers
 )
 
 // ReadInput retrieves the content of the input file
-func ReadInput(filepath string) (res []string) {
-	s, _ := os.Open(filepath)
-	sc := bufio.NewScanner(s)
-	for sc.Scan() {
-		// parsing here...
-		res = append(res, sc.Text())
-	}
-	return
+func ReadInput(filepath string) (res []int) {
+	raw, _ := os.ReadFile(filepath)
+	return Map(strings.Split(string(raw), "\n"), ToInt)
 }
 
-func Solve(input []string) (p1 int, p2 int) {
+func compute(n int) int {
+	k := ((n << 6) ^ n) & p
+	m := ((k >> 5) ^ k) // mask not needed here, all integers are 4bits or less
+	return ((m << 11) ^ m) & p
+}
+
+func predict(n int, cache map[[4]int]int) int {
+	current := n
+	diffs := NewQueue[int]()
+	seen := NewSet[[4]int]()
+	for range 2000 {
+		next := compute(current)
+		price := (next % 10)
+		diffs.Push(price - (current % 10))
+		if diffs.Len() > 4 {
+			diffs.Pop()
+		}
+		if diffs.Len() == 4 {
+			sequence := [4]int(diffs)
+			if !seen.Contains(sequence) {
+				cache[sequence] += price
+				seen.Add(sequence)
+			}
+		}
+		current = next
+	}
+
+	return current
+}
+
+func Solve(input []int) (p1 int, p2 int) {
+	cache := make(map[[4]int]int)
+	for _, n := range input {
+		p1 += predict(n, cache)
+	}
+
+	for _, v := range cache {
+		p2 = max(p2, v)
+	}
+
 	return
 }
 
 func TestDay22(t *testing.T) {
 	r := R.New(t)
-	p1Ex, p2Ex := Solve(ReadInput(example))
-	r.Equal(0, p1Ex, "example p1")
-	r.Equal(0, p2Ex, "example p2")
+	p1Ex, _ := Solve(ReadInput(example))
+	r.Equal(37327623, p1Ex, "example p1")
+	_, p2Ex := Solve([]int{1, 2, 3, 2024})
+	r.Equal(23, p2Ex, "example p2")
 	p1, p2 := Solve(ReadInput(input))
-	r.Equal(0, p1, "input p1")
-	r.Equal(0, p2, "input p2")
+	r.Equal(15613157363, p1, "input p1")
+	r.Equal(1784, p2, "input p2")
 }
 
 func BenchmarkDay22(b *testing.B) {
