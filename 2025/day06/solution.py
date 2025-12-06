15a79393de13f26b3af49074d309117f609a5bfe
Ashyaa <heimdall409@gmail.com>
2025-12-06 12:36:18 +0100

    2025: day 06

--- 2025/day06/solution.py
+++ 2025/day06/solution.py
@@ -1,39 +1,93 @@
 #!/usr/bin/env python3
 
 import contextlib
+from functools import reduce
 from pathlib import Path
-from typing import *
+from typing import List, Tuple
 
 from AoC.util import show
 
 CWD = Path(__file__).parent
 
+OP = {
+    "*": lambda x, y: x * y,
+    "+": lambda x, y: x + y,
+}
 
-def read_input(filename: str = "input.txt") -> None:
+
+def read_input(filename: str = "input.txt") -> Tuple[List[str], List[str]]:
     input_file = CWD.joinpath(filename)
+    res = []
     with open(input_file, "r") as reader:
-        pass
+        for line in reader.readlines():
+            res.append(line)
+    return res[:-1], res[-1].strip().split()
 
 
 @show
-def first(inp) -> None:
-    pass
+def first(numbers: List[str], operators: List[str]) -> int:
+    res = 0
+    nbs = [list(map(int, line.strip().split())) for line in numbers]
+    for i, op in enumerate(operators):
+        res += reduce(OP[op], (number[i] for number in nbs))
+    return res
+
+
+def str_to_numbers(read_nbs: List[str]) -> List[int]:
+    res = []
+    max_len = max(len(n) for n in read_nbs)
+    padded = [n + " " * (max_len - len(n)) for n in read_nbs]
+    for idx in range(len(padded[0])):
+        s = ""
+        for nb in padded:
+            s += nb[idx] if nb[idx] != " " else ""
+        if s:
+            res.append(int(s))
+    return res
+
+
+def p2_numbers(numbers: List[str], cur_idx: List[int]) -> Tuple[List[int], List[int]]:
+    res, new_idx = [], []
+    max_start_idx = max(cur_idx)
+    for i, line in enumerate(numbers):
+        start_idx = cur_idx[i]
+        found = False
+        s = ""
+        for idx in range(start_idx, len(line)):
+            char = line[idx]
+            s += char if idx >= max_start_idx else ""
+            if char == " " and found:
+                new_idx.append(idx)
+                res.append(s)
+                break
+            if char != " ":
+                found = True
+    return str_to_numbers(res), new_idx
 
 
 @show
-def second(inp) -> None:
-    pass
+def second(numbers: List[str], operators: List[str]) -> int:
+    res = 0
+    cur_idx = [0] * len(numbers)
+    max_len = max(len(n) for n in numbers)
+    ltr_numbers = [
+        (" " + n.replace("\n", "") + " " * (max_len - len(n)))[::-1] for n in numbers
+    ]
+    for op in reversed(operators):
+        nbs, cur_idx = p2_numbers(ltr_numbers, cur_idx)
+        res += reduce(OP[op], nbs)
+    return res
 
 
 def test_example() -> None:
+    inp = read_input("example.txt")
     with contextlib.redirect_stdout(None):
-        inp = read_input("example.txt")
-        assert first(inp) == None
-        assert second(inp) == None
+        assert first(*inp) == 4277556
+        assert second(*inp) == 3263827
 
 
 if __name__ == "__main__":
     test_example()
     inp = read_input()
-    first(inp)  # p1
-    second(inp)  # p2
+    first(*inp)  # 5595593539811
+    second(*inp)  # 10153315705125
